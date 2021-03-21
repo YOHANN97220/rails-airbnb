b39c037eea1d5cb4fca96f1d27bc3bdbc48f1a3c
yohann paulin <yohannpaulin@hotmail.fr>
2021-03-21 19:02:51 +0100

    added navbar and banner in index

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,12 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+  def show
+    @flat = Flat.find_by(id: params[:id])
+  end
+  # def new
+  # end
+
+  # def create
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: "flats#index"
-  resources :flats, only: [:index]
+  resources :flats, only: [:index, :show]
 end
